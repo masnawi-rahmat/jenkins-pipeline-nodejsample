e203f80f2cba2fb9fd078f96e30110631e8dc81c
Masnawi Rahmat <masnawi.rahmat@gmail.com>
2023-02-14 07:30:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+//File: index.js
+//const express = require('express');
+//const app = express();
+
+//app.get('/', (req, res) => {
+  res.send('Hello World!');
+//});
+
+//app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+//});
+
+//File: index.js
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 });
-
